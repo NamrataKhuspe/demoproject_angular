ad27cf5094d50b5510db671285d5c1efdb305191
unknown <namrata.khuspe18@gmail.com>
2021-10-05 12:05:01 +0530

    envfile

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  "local_bakendurl": "http://127.0.0.1:8080/"
+  production: true,
+  // "local_bakendurl": "http://127.0.0.1:8080/"
 };
 
 /*
